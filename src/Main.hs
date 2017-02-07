f2cc15f6d8b259789f6e44b917e2f2121781859e
alex <alexnlarsen@gmail.com>
2017-02-07 12:27:11 +0100

    lul

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
+import System.Environment
+
+evaluateArgs :: [String] -> IO ()
+evaluateArgs [] = putStrLn "Usage: -{command name} -n {n1, n2, n3} -{completion}"
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
-  putStrLn "additioN"
+  args <- getArgs
+  evaluateArgs args
+  putStrLn "bye"
