7941c6f10baaacfea34ac6ae232335fa1561064b
alex <alexnlarsen@gmail.com>
2017-02-08 16:50:25 +0100

    Added functionality to parse command line arguments

--- src/Main.hs
+++ src/Main.hs
@@ -6,12 +6,11 @@ import Data.List
 evaluateArgs :: [String] -> IO ()
 evaluateArgs [] = putStrLn "Usage: -[a]ction{add, remove} -[c]ommand{command name} -argume[n]t {argument number x1, x2, x3, n (n = all > x3)} -{\"completion\"}"
 --evaluateArgs (x:xs) = putStrLn $ "Adding " ++ completion ++ " to command " ++ command ++ " as argument numbers: " ++ numbers where
-evaluateArgs xs = putStrLn $ "Completion: " ++ completion ++ "command: " ++ command ++ "numbers: " ++ numbers where
-    getArgs = "hello"
-    separateArgs = words $ concat xs
-    completion = "bruh"
-    command = "hi"
-    numbers = intercalate ", " $ foldr (\x acc -> [x] : acc) [] "123"
+evaluateArgs xs = putStrLn $ "Completion: " ++ completion ++ " - command: " ++ command ++ " - numbers: " ++ numbers where
+    completion = getFlag "-c" 1
+    command = getFlag "-a" 1
+    numbers = takeWhile (/= '-') $ getFlag "-n" (maxBound :: Int)
+    getFlag flag n = concat . take n . drop 1 $ dropWhile (/= flag) xs
 
 
 
