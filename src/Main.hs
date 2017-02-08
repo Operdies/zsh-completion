f5154ff772bb86a7a26d7731dc9b007f4ca82ff2
alex <alexnlarsen@gmail.com>
2017-02-08 16:25:00 +0100

    Just testing git tool

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,19 @@
 module Main where
 
 import System.Environment
+import Data.List
 
 evaluateArgs :: [String] -> IO ()
-evaluateArgs [] = putStrLn "Usage: -{command name} -n {n1, n2, n3} -{completion}"
+evaluateArgs [] = putStrLn "Usage: -[a]ction{add, remove} -[c]ommand{command name} -argume[n]t {argument number x1, x2, x3, n (n = all > x3)} -{\"completion\"}"
+--evaluateArgs (x:xs) = putStrLn $ "Adding " ++ completion ++ " to command " ++ command ++ " as argument numbers: " ++ numbers where
+evaluateArgs xs = putStrLn $ "Completion: " ++ completion ++ "command: " ++ command ++ "numbers: " ++ numbers where
+    getArgs = "hello"
+    separateArgs = words $ concat xs
+    completion = "bruh"
+    command = "hi"
+    numbers = intercalate ", " $ foldr (\x acc -> [x] : acc) [] "123"
+
+
 
 main :: IO ()
 main = do
